938d4135861c0eca76a548d0f8dba0f1e110cd84
Jorge Martin <jorgemaram@gmail.com>
2021-01-30 13:03:08 +0100

    news display

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Header from './components/Header'
 import Formulario from './components/Formulario'
+import ListadoNoticias from './components/ListadoNoticias'
 
 function App() {
+
+
+  //definir categoría y noticia
+  const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=gb&category=${categoria}&apiKey=2a90a3137fb24faabaf2fd3983872ed7`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles)
+    }
+    consultarAPI();
+  }, [categoria]);
+
   return (
     <>
       <Header
         titulo='Buscador de Noticias'
       />
       <div className='container white'>
-        <Formulario />
+        <Formulario guardarCategoria={guardarCategoria} />
+        <ListadoNoticias noticias={noticias} />
       </div>
     </>
   );
