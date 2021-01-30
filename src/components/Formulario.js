8abed1bfe1ea7c021dbcd24eff1bf25c931e193d
Jorge Martin <jorgemaram@gmail.com>
2021-01-30 12:09:31 +0100

    custom hook and form creation

---
+++ src/components/Formulario.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from './Formulario.module.css'
+import useSelect from '../hooks/useSelect'
+
+const Formulario = () => {
+
+    const OPCIONES = [
+        {value: 'general', label: 'General'},
+        {value: 'business', label: 'Negocios'},
+        {value: 'entertainment', label: 'Entretenimiento'},
+        {value: 'health', label: 'Salud'},
+        {value: 'science', label: 'Ciencia'},
+        {value: 'sports', label: 'Deportes'},
+        {value: 'technology', label: 'Tecnología'}
+    ]
+
+    //utilizar custom hook
+    const [categoria, SelectNoticias] = useSelect('general', OPCIONES);
+
+
+    return ( 
+        <div className={`${styles.buscador} row`}>
+            <div className='col s12 m8 offset-m2'>
+                <form>
+                    <h2 className={styles.heading}>Encuentra tus noticias por categoría</h2>
+
+                    <SelectNoticias />
+
+                    <div className='input-field col s12'>
+                        <input type='submit' className={`${styles.['btn-block']} btn-large amber darken-2`} value='Buscar'/>
+                    </div>
+                </form>
+            </div>
+        </div>
+     );
+}
+ 
+export default Formulario;
